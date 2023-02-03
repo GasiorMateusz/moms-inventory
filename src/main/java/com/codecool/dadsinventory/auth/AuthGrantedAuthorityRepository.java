b8d1efd074cccb6b66aee9c3d12cf78803055d16
GasiorMateusz <mateuszgasior314@gmail.com>
2023-02-03 17:46:47 +0100

    remove auth files from tut

--- src/main/java/com/codecool/dadsinventory/auth/AuthGrantedAuthorityRepository.java
+++
@@ -1,6 +0,0 @@
-package com.codecool.dadsinventory.auth;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AuthGrantedAuthorityRepository extends JpaRepository<AuthGrantedAuthority, Long> {
-}
